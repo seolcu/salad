eb4fb05cd023920b8dd4dd3a65f08255f19744b9
Gyuwon Seol <seolcu0112@proton.me>
2024-12-07 05:16:13 +0900

    lcd 커서 숨기기

--- actuators/lcd.py
+++ actuators/lcd.py
@@ -19,6 +19,7 @@ def init_lcd():
     lcd = CharLCD(
         i2c_expander="PCF8574", address=0x27, port=1, cols=16, rows=2, dotsize=8
     )
+    lcd.cursor_mode = "hide"
     lcd.clear()
     return lcd
 
