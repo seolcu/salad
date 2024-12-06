0a1507ab079b7027893b233b85abe6acc4f14ba3
Gyuwon Seol <seolcu0112@proton.me>
2024-12-07 05:35:43 +0900

    infrared -> motion으로 이름 수정

--- sensors/motionsensor.c
+++ sensors/motionsensor.c
@@ -8,32 +8,40 @@ volatile int eventCounter = 0; // Event counter for motion detection
 #define MOTION_IN 0
 
 // Interrupt service routine
-void myInterrupt(void) {
+void myInterrupt(void)
+{
     eventCounter++;
 }
 
-int main(void) {
+int main(void)
+{
     // Initialize WiringPi
-    if (wiringPiSetup() < 0) {
+    if (wiringPiSetup() < 0)
+    {
         printf("Unable to setup wiringPi\n");
         return 1;
     }
 
     // Set pin mode and attach interrupt
     pinMode(MOTION_IN, INPUT);
-    if (wiringPiISR(MOTION_IN, INT_EDGE_RISING, &myInterrupt) < 0) {
+    if (wiringPiISR(MOTION_IN, INT_EDGE_RISING, &myInterrupt) < 0)
+    {
         printf("Unable to setup ISR\n");
         return 1;
     }
 
-    printf("Monitoring infrared sensor...\n");
+    printf("Monitoring motion sensor...\n");
 
-    while (1) {
-        if (digitalRead(MOTION_IN) > 0) {
+    while (1)
+    {
+        if (digitalRead(MOTION_IN) > 0)
+        {
             printf("Infrared Sensor Detected! Total Events: %d\n", eventCounter);
             eventCounter = 0; // Reset counter after processing
-            //사람 감지 시 수행할 로직 작성 부분
-        } else {
+            // 사람 감지 시 수행할 로직 작성 부분
+        }
+        else
+        {
             printf("No motion detected.\n");
         }
         delay(500); // Delay for 0.5 seconds
