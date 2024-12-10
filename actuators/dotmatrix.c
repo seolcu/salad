a8fff786e1101ef95e2230fccefa0923179e3ad2
Gyuwon Seol <seolcu0112@proton.me>
2024-12-10 17:26:29 +0900

    도트매트릭스 안 쓰는 코드 주석처리

--- actuators/dotmatrix.c
+++ actuators/dotmatrix.c
@@ -24,42 +24,42 @@ unsigned char sad_pattern[8] = {
     0b10100101,
     0b01000010,
     0b00111100};
-unsigned char neutral_pattern[8] = {
-    0b00111100,
-    0b01000010,
-    0b10100101,
-    0b10000001,
-    0b10000001,
-    0b10111101,
-    0b01000010,
-    0b00111100};
-unsigned char kim_pattern[8] = {
-    0b11111001,
-    0b00001001,
-    0b00001001,
-    0b00001001,
-    0b00000000,
-    0b11111111,
-    0b10000001,
-    0b11111111};
-unsigned char yeong_pattern[8] = {
-    0b01100001,
-    0b10010111,
-    0b10010001,
-    0b01100111,
-    0b00000001,
-    0b01111110,
-    0b10000001,
-    0b01111110};
-unsigned char min_pattern[8] = {
-    0b11111001,
-    0b10001001,
-    0b10001001,
-    0b11111001,
-    0b00000000,
-    0b10000000,
-    0b10000000,
-    0b11111111};
+// unsigned char neutral_pattern[8] = {
+//     0b00111100,
+//     0b01000010,
+//     0b10100101,
+//     0b10000001,
+//     0b10000001,
+//     0b10111101,
+//     0b01000010,
+//     0b00111100};
+// unsigned char kim_pattern[8] = {
+//     0b11111001,
+//     0b00001001,
+//     0b00001001,
+//     0b00001001,
+//     0b00000000,
+//     0b11111111,
+//     0b10000001,
+//     0b11111111};
+// unsigned char yeong_pattern[8] = {
+//     0b01100001,
+//     0b10010111,
+//     0b10010001,
+//     0b01100111,
+//     0b00000001,
+//     0b01111110,
+//     0b10000001,
+//     0b01111110};
+// unsigned char min_pattern[8] = {
+//     0b11111001,
+//     0b10001001,
+//     0b10001001,
+//     0b11111001,
+//     0b00000000,
+//     0b10000000,
+//     0b10000000,
+//     0b11111111};
 
 void pin_setup()
 {
@@ -106,34 +106,34 @@ void draw_sad()
         send_command(row + 1, sad_pattern[row]);
     }
 }
-void draw_neutral()
-{
-    for (int row = 0; row < 8; row++)
-    {
-        send_command(row + 1, neutral_pattern[row]);
-    }
-}
-void draw_kim()
-{
-    for (int row = 0; row < 8; row++)
-    {
-        send_command(row + 1, kim_pattern[row]);
-    }
-}
-void draw_yeong()
-{
-    for (int row = 0; row < 8; row++)
-    {
-        send_command(row + 1, yeong_pattern[row]);
-    }
-}
-void draw_min()
-{
-    for (int row = 0; row < 8; row++)
-    {
-        send_command(row + 1, min_pattern[row]);
-    }
-}
+// void draw_neutral()
+// {
+//     for (int row = 0; row < 8; row++)
+//     {
+//         send_command(row + 1, neutral_pattern[row]);
+//     }
+// }
+// void draw_kim()
+// {
+//     for (int row = 0; row < 8; row++)
+//     {
+//         send_command(row + 1, kim_pattern[row]);
+//     }
+// }
+// void draw_yeong()
+// {
+//     for (int row = 0; row < 8; row++)
+//     {
+//         send_command(row + 1, yeong_pattern[row]);
+//     }
+// }
+// void draw_min()
+// {
+//     for (int row = 0; row < 8; row++)
+//     {
+//         send_command(row + 1, min_pattern[row]);
+//     }
+// }
 
 void dotmatrix_init()
 {
--- actuators/dotmatrix.h
+++ actuators/dotmatrix.h
@@ -3,10 +3,10 @@
 
 void draw_smile();
 void draw_sad();
-void draw_neutral();
-void draw_kim();
-void draw_yeong();
-void draw_min();
+// void draw_neutral();
+// void draw_kim();
+// void draw_yeong();
+// void draw_min();
 void dotmatrix_init();
 void clear();
 
